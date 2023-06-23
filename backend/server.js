b58057e960902e9daabeab41d698591a6e83893a
Tarun-kasarapu <ktarunkasaraput@gmail.com>
2023-06-23 10:23:39 +0530

    added filename

--- backend/server.js
+++ backend/server.js
@@ -100,12 +100,13 @@ app.post('/',async function(req,res){
 
 console.log("HIi");
 console.log(__dirname);
+const filename=path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.use(express.static(path.join(filename, '/frontend/build')))
   
     app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+      res.sendFile(path.resolve(filename, 'frontend', 'build', 'index.html'))
     )
   } else {
     app.get('/', (req, res) => {
